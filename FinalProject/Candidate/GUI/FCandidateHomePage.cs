c4ebe2aa6f2f2c65f51cad561e397ee628e24443
2thuancr <22110006@student.hcmute.edu.vn>
2024-03-03 23:16:51 +0700

    Update HomePage, ChangePassword, QlyCV

---
+++ FinalProject/Candidate/GUI/FCandidateHomePage.cs
@@ -0,0 +1,58 @@
+﻿using FinalProject.Common.GUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject.Candidate.GUI
+{
+    public partial class FCandidateHomePage : Form
+    {
+        public FCandidateHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void FCandidateHomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_TrangCaNhan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_CongTy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_HoSo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_ViecLam_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuItem_ThongTinCaNhan_Click(object sender, EventArgs e)
+        {
+            FCandidateInfor fCandidateInfor = new FCandidateInfor();
+            fCandidateInfor.ShowDialog();
+        }
+
+        private void menuItem_DoiMatKhau_Click(object sender, EventArgs e)
+        {
+            FChangePassword fChangePassword = new FChangePassword();
+            fChangePassword.ShowDialog();
+        }
+    }
+}
---
+++ FinalProject/Candidate/GUI/FQlyCv.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace FinalProject.Candidate.GUI
+{
+    partial class FQlyCv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox_TrangThai = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer1.Size = new System.Drawing.Size(981, 605);
+            this.splitContainer1.SplitterDistance = 613;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox1.Controls.Add(this.textBox_Name);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox1.Location = new System.Drawing.Point(30, 147);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(415, 75);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tên CV";
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Name.Location = new System.Drawing.Point(10, 33);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(395, 30);
+            this.textBox_Name.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.groupBox2.Controls.Add(this.comboBox_TrangThai);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox2.Location = new System.Drawing.Point(30, 228);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox2.Size = new System.Drawing.Size(415, 75);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Trạng thái";
+            // 
+            // comboBox_TrangThai
+            // 
+            this.comboBox_TrangThai.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox_TrangThai.FormattingEnabled = true;
+            this.comboBox_TrangThai.Location = new System.Drawing.Point(10, 33);
+            this.comboBox_TrangThai.Name = "comboBox_TrangThai";
+            this.comboBox_TrangThai.Size = new System.Drawing.Size(395, 33);
+            this.comboBox_TrangThai.TabIndex = 3;
+            // 
+            // FQlyCv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 605);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FQlyCv";
+            this.Text = "FQlyCv";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.ComboBox comboBox_TrangThai;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
