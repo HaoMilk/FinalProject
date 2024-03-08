07a6aeb7e61a640a7511ac42263f71ec8c79e054
2ThuanCr <2ThuanCr@gmail.com>
2024-03-08 10:05:54 +0700

    add UCJobItem

--- FinalProject/Properties/Resources.Designer.cs
+++ FinalProject/Properties/Resources.Designer.cs
@@ -704,6 +704,16 @@ namespace FinalProject.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap w04 {
+            get {
+                object obj = ResourceManager.GetObject("w04", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap warning {
             get {
                 object obj = ResourceManager.GetObject("warning", resourceCulture);
