b8ce22ef3568a6a57440a6c1d2267542722e0a93
khanhne1304 <chikhanhphanle2004@gmail.com>
2024-04-05 00:41:52 +0700

    update admin ThongKe

--- FinalProject/Admin/FAdminHomePage.Designer.cs
+++ FinalProject/Admin/FAdminHomePage.Designer.cs
@@ -264,6 +264,7 @@
             this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FAdminHomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FAdminHomePage";
             this.menuStrip_MenuStrip.ResumeLayout(false);
             this.menuStrip_MenuStrip.PerformLayout();
