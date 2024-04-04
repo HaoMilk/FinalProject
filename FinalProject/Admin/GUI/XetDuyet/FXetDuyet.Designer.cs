181eaa76ec67e85536d6669740f39eb4c4977918
khanhne1304 <chikhanhphanle2004@gmail.com>
2024-04-05 01:28:01 +0700

    add Uc admin

--- FinalProject/Admin/GUI/XetDuyet/FXetDuyet.Designer.cs
+++ FinalProject/Admin/GUI/XetDuyet/FXetDuyet.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1173, 479);
+            this.ClientSize = new System.Drawing.Size(1173, 450);
             this.Name = "FXetDuyet";
             this.Text = "FXetDuyet";
             this.ResumeLayout(false);
