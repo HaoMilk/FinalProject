c17a8b14685624d557a740aaac161e931e1d3648
2ThuanCr <2thuancr03@gmail.com>
2024-04-16 23:48:22 +0700

    Update UI Candidate info

--- FinalProject/Candidate/GUI/FCandidateInfor.cs
+++ FinalProject/Candidate/GUI/FCandidateInfor.cs
@@ -1,11 +1,13 @@
 ﻿using FinalProject.Common;
 using FinalProject.Common.BUS;
+using FinalProject.Common.Helper;
 using FinalProject.UC;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,15 @@ namespace FinalProject.Candidate.GUI
         public FCandidateInfor()
         {
             InitializeComponent();
+
+            var items = new ComboBoxItem[]
+            {
+                new ComboBoxItem { Text = "Nam", Value = "M" },
+                new ComboBoxItem { Text = "Nữ", Value = "F" },
+                new ComboBoxItem { Text = "Khác", Value = "O" },
+            };
+            this.ucComboBox_Gender.Items = items;
+            this.ucComboBox_Gender.SelectedIndex = 0;
         }
 
         private void button_Save_Click(object sender, EventArgs e)
@@ -26,11 +37,11 @@ namespace FinalProject.Candidate.GUI
             var id = LoggedUser.UngVien.Id;
             var hoten = textBox_Name.Text;
             var ngaysinh = dateTime_Birthday.Value;
-            var gioitinh = textBox_Gender.Text;
+            var gioitinh = ucComboBox_Gender.Text;
             var diachi = textBox_Address.Text;
             var sdt = textBox_Phone.Text;
             var email = textBox_Email.Text;
-            var chuyenmon = textBox_Major.Text;
+            var chuyenmon = richTextBox_ChuyenMon.Text;
             var trangthai = LoggedUser.UngVien.TrangThai;
             var avatar = LoggedUser.UngVien.Avatar;
 
@@ -54,13 +65,12 @@ namespace FinalProject.Candidate.GUI
             {
                 textBox_Name.Text = ungVien.HoTen;
                 dateTime_Birthday.Value = ungVien.NgaySinh;
-                textBox_Gender.Text = ungVien.GioiTinh;
+                ucComboBox_Gender.Text = ungVien.GioiTinh;
                 textBox_Address.Text = ungVien.DiaChi;
                 textBox_Phone.Text = ungVien.SDT;
                 textBox_Email.Text = ungVien.Email;
-                textBox_Major.Text = ungVien.ChuyenMon;
+                richTextBox_ChuyenMon.Text = ungVien.ChuyenMon;
             }
-         
         }
     }
 }
