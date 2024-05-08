9fe80091a04b81db21d7b7884b76af0165640a62
2ThuanCr <2ThuanCr03@gmail.com>
2024-05-08 15:54:07 +0700

    Gui mail từ chối hồ sơ

--- FinalProject/Company/GUI/FViewCV.cs
+++ FinalProject/Company/GUI/FViewCV.cs
@@ -220,6 +220,7 @@ namespace FinalProject.Company.GUI
                 UCMessageBox.Show("Vui lòng nhập thông tin kết quả phỏng vấn!");
                 return;
             }
+            _ut.KetQuaPhongVan = richTextBox_KetQuaPV.Text;
 
             //result = _utBUS.UpdateTrangThai(ID, trangThai);
             var ungTuyen = new UngTuyen
@@ -231,8 +232,16 @@ namespace FinalProject.Company.GUI
             result = _utBUS.Update(ungTuyen);
             if (result > 0)
             {
-                UCMessageBox.Show("Loại bỏ thành công");
-                this.Close();
+                var output = ungTuyen.SendEmailTuChoiHoSoPhongVan(_ut);
+                if (output.IsSuccess)
+                {
+                    UCMessageBox.Show("Loại bỏ thành công");
+                    this.Close();
+                }
+                else
+                {
+                    UCMessageBox.Show("Có lỗi phát sinh khi gửi email.");
+                }
             }
             else
             {
