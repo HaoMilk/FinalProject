3b11bd7fb23f53412abfa722e21d0c314acdb4e0
2ThuanCr <2ThuanCr03@gmail.com>
2024-05-09 17:38:59 +0700

    `EF` - CongViecBUS

--- FinalProject/BUS/CongViecBUS.cs
+++ FinalProject/BUS/CongViecBUS.cs
@@ -1,26 +1,176 @@
 ﻿using FinalProject.Common.DTO;
+using FinalProject.Common.Helper;
 using FinalProject.DAO;
 using FinalProject.Database;
 using FinalProject.UC;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace FinalProject.BUS
 {
     public class CongViecBUS
     {
-        private CongViecDAO congViecDAO = new CongViecDAO();
+        #region EF6
         public List<CongViec> Search(CongViecGetAllInput input)
         {
-            return congViecDAO.GetAll(input);
+            using (var db = new JobManagerContext())
+            {
+                var query = db.CongViec
+                    .Where(x => x.IsDeleted == false)
+                    .WhereIf(
+                        !string.IsNullOrEmpty(input.Search),
+                        x => x.TenCongTy.Contains(input.Search) || x.ViTriTuyenDung.Contains(input.Search))
+                    .WhereIf(!string.IsNullOrWhiteSpace(input.DiaDiem), x => x.DiaDiem.Contains(input.DiaDiem))
+                    .WhereIf(!string.IsNullOrWhiteSpace(input.Nganh), x => x.Nganh.Contains(input.Nganh))
+                    .WhereIf(!string.IsNullOrWhiteSpace(input.KinhNghiem), x => x.KinhNghiem.Contains(input.KinhNghiem))
+                    .WhereIf(input.MinLuong != null, x => x.MucLuong >= input.MinLuong)
+                    .WhereIf(input.MaxLuong != null, x => x.MucLuong <= input.MaxLuong)
+                    .WhereIf(!string.IsNullOrWhiteSpace(input.GioiTinh), x => x.GioiTinh == input.GioiTinh)
+                    .WhereIf(input.FromDate != null, x => x.FromDate >= input.FromDate)
+                    .WhereIf(input.ToDate != null, x => x.ToDate <= input.ToDate)
+                    .WhereIf(input.IdCongTy != null, x => x.IdCongTy == input.IdCongTy)
+                    .WhereIf(input.FromId != 0, x => x.Id >= input.FromId)
+                    .WhereIf(input.ToId != 0, x => x.Id <= input.ToId);
+
+                return query.ToList();
+            }
         }
 
         public int CheckExist(int id)
         {
-            return congViecDAO.CheckExist(id);
+            using (var db = new JobManagerContext())
+            {
+                return db.CongViec.Count(x => x.Id == id);
+            }
         }
 
         public int Add(string tenCongTy, int IdCongTy, string ten, string nganh, string viTriTuyenDung,
+            decimal mucLuong, string diaDiem, string trinhDoHocVan,
+            string gioiTinh, string lienHe, string moTa,
+            string quyenLoi, string kinhNghiem, string yeuCauUngVien, int soLuong, DateTime fromdate)
+        {
+            try
+            {
+                using (var db = new JobManagerContext())
+                {
+                    var ct = new CongViec();
+                    ct.TenCongTy = tenCongTy;
+                    ct.IdCongTy = IdCongTy;
+                    ct.Ten = ten;
+                    ct.Nganh = nganh;
+                    ct.ViTriTuyenDung = viTriTuyenDung;
+                    ct.MucLuong = mucLuong;
+                    ct.DiaDiem = diaDiem;
+                    ct.TrinhDoHocVan = trinhDoHocVan;
+                    ct.GioiTinh = gioiTinh;
+                    ct.LienHe = lienHe;
+                    ct.MoTa = moTa;
+                    ct.QuyenLoi = quyenLoi;
+                    ct.KinhNghiem = kinhNghiem;
+                    ct.YeuCauUngVien = yeuCauUngVien;
+                    ct.SoLuong = soLuong;
+                    ct.IsDeleted = false;
+                    ct.CreatedTime = DateTime.Now;
+                    ct.UpdatedTime = DateTime.Now;
+                    ct.FromDate = fromdate;
+                    db.CongViec.Add(ct);
+                    return db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                UCMessageBox.Show(ex.Message);
+                return 0;
+            }
+        }
+
+        public int Update(int Id, int idCongTy, string tenCongTy, string ten, string nganh, string viTriTuyenDung,
+            decimal mucLuong, string diaDiem, string trinhDoHocVan,
+            string gioiTinh, string lienHe, string moTa,
+            string quyenLoi, string kinhNghiem, string yeuCauUngVien, int soLuong, DateTime fromdate)
+        {
+            try
+            {
+                using (var db = new JobManagerContext())
+                {
+                    var ct = db.CongViec.Find(Id);
+                    ct.IdCongTy = idCongTy;
+                    ct.TenCongTy = tenCongTy;
+                    ct.Ten = ten;
+                    ct.Nganh = nganh;
+                    ct.ViTriTuyenDung = viTriTuyenDung;
+                    ct.MucLuong = mucLuong;
+                    ct.DiaDiem = diaDiem;
+                    ct.TrinhDoHocVan = trinhDoHocVan;
+                    ct.GioiTinh = gioiTinh;
+                    ct.LienHe = lienHe;
+                    ct.MoTa = moTa;
+                    ct.QuyenLoi = quyenLoi;
+                    ct.KinhNghiem = kinhNghiem;
+                    ct.YeuCauUngVien = yeuCauUngVien;
+                    ct.SoLuong = soLuong;
+                    ct.UpdatedTime = DateTime.Now;
+                    ct.FromDate = fromdate;
+                    return db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                UCMessageBox.Show(ex.Message);
+                return 0;
+            }
+        }
+
+        public int Delete(int id)
+        {
+            using (var db = new JobManagerContext())
+            {
+                var congViec = db.CongViec.Find(id);
+                db.CongViec.Remove(congViec);
+                return db.SaveChanges();
+            }
+        }
+
+        public int SoftDelete(int id)
+        {
+            using (var db = new JobManagerContext())
+            {
+                var congViec = db.CongViec.Find(id);
+                congViec.IsDeleted = true;
+                congViec.UpdatedTime = DateTime.Now;
+                return db.SaveChanges();
+            }
+        }
+        public CongViec GetById(int id)
+        {
+            using (var db = new JobManagerContext())
+            {
+                return db.CongViec.Find(id);
+            }
+        }
+        public List<CongViec> GetByIDCty(CongViecGetAllInput input, int comanyId)
+        {
+            using (var db = new JobManagerContext())
+            {
+                return db.CongViec.Where(x => x.IdCongTy == comanyId).ToList();
+            }
+        }
+        #endregion EF6
+
+        #region DAO
+        private CongViecDAO congViecDAO = new CongViecDAO();
+        public List<CongViec> SearchDAO(CongViecGetAllInput input)
+        {
+            return congViecDAO.GetAll(input);
+        }
+
+        public int CheckExistDAO(int id)
+        {
+            return congViecDAO.CheckExist(id);
+        }
+
+        public int AddDAO(string tenCongTy, int IdCongTy, string ten, string nganh, string viTriTuyenDung,
             decimal mucLuong, string diaDiem, string trinhDoHocVan,
             string gioiTinh, string lienHe, string moTa,
             string quyenLoi, string kinhNghiem, string yeuCauUngVien, int soLuong, DateTime fromdate)
@@ -63,7 +213,7 @@ namespace FinalProject.BUS
             }
         }
 
-        public int Update(int Id, int idCongTy, string tenCongTy, string ten, string nganh, string viTriTuyenDung,
+        public int UpdateDAO(int Id, int idCongTy, string tenCongTy, string ten, string nganh, string viTriTuyenDung,
             decimal mucLuong, string diaDiem, string trinhDoHocVan,
             string gioiTinh, string lienHe, string moTa,
             string quyenLoi, string kinhNghiem, string yeuCauUngVien, int soLuong, DateTime fromdate)
@@ -105,14 +255,14 @@ namespace FinalProject.BUS
             }
         }
 
-        public int Delete(int id)
+        public int DeleteDAO(int id)
         {
             var congViec = new CongViec();
             congViec.Id = id;
             return congViecDAO.Delete(congViec);
         }
 
-        public int SoftDelete(int id)
+        public int SoftDeleteDAO(int id)
         {
             var congViec = new CongViec();
             congViec.Id = id;
@@ -121,13 +271,14 @@ namespace FinalProject.BUS
 
             return congViecDAO.SoftDelete(congViec);
         }
-        public CongViec GetById(int id)
+        public CongViec GetByIdDAO(int id)
         {
             return congViecDAO.GetById(id);
         }
-        public List<CongViec> GetByIDCty(CongViecGetAllInput input, int comanyId)
+        public List<CongViec> GetByIDCtyDAO(CongViecGetAllInput input, int comanyId)
         {
             return congViecDAO.GetByIDCty(input, comanyId);
         }
+        #endregion DAO
     }
 }
---
+++ FinalProject/Common/Helper/QueryableExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Common.Helper
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> Page<T>(this IQueryable<T> query, int page, int pageSize)
+        {
+            return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Func<T, bool> predicate)
+        {
+            return condition ? query.Where(predicate).AsQueryable() : query;
+        }
+
+        public static IQueryable<T> OrderByIf<T>(this IQueryable<T> query, bool condition, Func<T, object> keySelector)
+        {
+            return condition ? query.OrderBy(keySelector).AsQueryable() : query;
+        }
+
+        public static string ToSql<T>(this IQueryable<T> query)
+        {
+            var sql = query.ToString();
+            return sql;
+        }
+    }
+}
